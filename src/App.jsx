9a0ff575b7951bcaa573d44c2ebb9453a7a3e0e4
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gmail.com>
2023-04-03 17:57:29 +0100

    Passing methods(functions) as properties

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import EventBind from "./components/EventBind";
+import ParentComponent from "./components/ParentComponent";
 
 function App() {
   return (
     <div className="App">
-      <EventBind />
+      <ParentComponent />
     </div>
   );
 }
