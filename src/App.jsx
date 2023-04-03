bc35f0076c68e9a3257f9f9fbb1aec732b8aa8fc
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gmail.com>
2023-04-03 14:15:18 +0100

    State implementation

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Greet from "./components/Greet";
-import Classgreet from "./components/Classgreet";
+import Message from "./components/Message";
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Bruce" heroName="Batman">
-        <p>This is children props</p>
-      </Greet>
-      <Classgreet name="Bruce" heroName="Batman" />
+      <Message />
     </div>
   );
 }
