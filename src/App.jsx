f33e38b73093b38d5c00971c4682ef3fb2f7198d
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gmail.com>
2023-04-04 16:48:19 +0100

    Basic for handling in React Class Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,13 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import "./appStyles.css";
-import styles from "./appStyles.module.css";
-import Stylesheet from "./components/Stylesheet.jsx";
-import Inline from "./components/Inline";
+import "./index.css";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
-      {/* <Stylesheet primary={true} /> */}
-      <h1 className="error">Error</h1>
-      <h1 className={styles.success}>Success</h1>
-      <Inline />
+      <Form />
     </div>
   );
 }
