f145eca767eac1f933b2404ca443eb2a5a3469e5
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gmail.com>
2023-04-04 13:44:04 +0100

    Uing index as key

--- src/components/NameList.jsx
+++ src/components/NameList.jsx
@@ -2,30 +2,14 @@ import React from "react";
 import Person from "./Person";
 
 const NameList = () => {
-  const persons = [
-    {
-      id: 1,
-      name: "Bruce",
-      age: 30,
-      skill: "React",
-    },
-    {
-      id: 2,
-      name: "Bruce",
-      age: 30,
-      skill: "Angular",
-    },
-    {
-      id: 3,
-      name: "Diana",
-      age: 28,
-      skill: "Vue",
-    },
-  ];
-  const personList = persons.map((person) => (
-    <Person key={person.id} person={person} />
+  const names = ["Bruce", "Clark", "Diana"];
+  const persons = [];
+  const nameList = names.map((name, index) => (
+    <h2 key={index}>
+      {index} {name}
+    </h2>
   ));
-  return <div>{personList}</div>;
+  return <div>{nameList}</div>;
 };
 
 export default NameList;
